1d8f916925ea515031837e6d29a8de3cffc4f1ed
ohadbachner <ohad.bachner@campus.technion.ac.il>
2022-04-11 13:55:12 +0300

    update!

--- HW1/main.c
+++ HW1/main.c
@@ -3,9 +3,9 @@ int bitAnd(int x, int y);
 int getByte(int x, int n);
 int logicalShift(int x, int n);
 int main() {
-   // printf(bitAnd(6,5));
-    printf("0x%x",getByte(0x12345678,1));
-    //printf("%x", logicalShift(0x87654321,4));
+    printf("%d\n",bitAnd(6,5));
+    printf("0x%x\n",getByte(0x12345678,1));
+    printf("0x%x", logicalShift(0x87654321,4));
     return 0;
 
 }
@@ -18,7 +18,12 @@ int getByte(int x, int n) {
     return (x >> 8*n) & 0xFF;
 }
 int logicalShift(int x, int n){
-    return (x >> n);
+    int mask = ~2 + 1;
+    int shiftAmount = 31 + ((~n) + 1);//this evaluates to 31 - n on two's complement machines
+    mask = mask << shiftAmount;
+    mask = ~mask;//If n equals 0, it means we have negated all bits and hence have mask = 0
+    x = x >> n;
+    return x & mask;
 }
 
 
